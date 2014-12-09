f6a6440004adc5de14cdb4c92daf0b661bdcd2c2
Denis Stepulenok <super.denis@gmail.com>
2014-12-09 21:59:30 +0300

    Сервлет вычисляет сумму двух чисел

---
+++ src/main/java/mybeans/Point.java
@@ -0,0 +1,24 @@
+package mybeans;
+
+/**
+ *
+ */
+public class Point {
+    double x, y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
---
+++ src/main/java/servlets/MyServlet.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ *
+ */
+public class MyServlet extends javax.servlet.http.HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    /**
+     * Ответ на GET-запрос
+     *
+     * @param request
+     * @param response
+     * @throws javax.servlet.ServletException
+     * @throws IOException
+     */
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("Protocol: " + request.getProtocol());
+
+        response.setCharacterEncoding("UTF-8");
+        ServletOutputStream out = response.getOutputStream();
+
+        Map<String, String[]> map = request.getParameterMap();
+        if (map.size() == 2) {
+            double a = Double.parseDouble(request.getParameter("a"));
+            double b = Double.parseDouble(request.getParameter("b"));
+            out.println("Sum: " + a + " + " + b + " = " + (a + b));
+        }
+
+        out.println("Protocol: " + request.getProtocol());
+        out.println("URI: " + request.getRequestURI());
+    }
+}
